173e2631e312db009ce60e9cca66f940ca2cfa97
VirtualOx <tvirtualox@gmail.com>
2022-07-30 18:01:02 -0500

    Update

--- content.js
+++ content.js
@@ -1,186 +1,198 @@
-var HTML = document.documentElement.innerHTML;
-var width = 0;
+const query = qry => document.body.querySelector(qry)
+var preservedState = null
 
-//función que toma algo dentro del html.
+//Función que obtiene algo dentro de html.
 function pegaString(str, first_character, last_character) {
-	if(str.match(first_character + "(.*)" + last_character) == null){
-		return null;
-	}else{
-	    new_str = str.match(first_character + "(.*)" + last_character)[1].trim()
-	    return(new_str)
-    }
+  if (str.match(first_character + "(.*)" + last_character) == null) {
+    return null;
+  } else {
+    new_str = str.match(first_character + "(.*)" + last_character)[1].trim()
+    return new_str;
+  }
 }
 
-//function que optimiza la página para dispositivos móviles.
-function optimize_for_mobile() {
+//Función para eliminar elementos de la página
+function remove(element, name, untilRemoved = false, callback = () => { }) {
+  let tries = 0;
+  if (untilRemoved) {
+    const finishRemove = setInterval(() => {
+      if (query(element) != null) {
+        clearInterval(finishRemove)
+        console.log(`[CR Premium] Quitando ${name}...`);
+        const closeBtn = query(element + ' > .close-button')
+        if (closeBtn) closeBtn.click()
+        else query(element).style.display = 'none';
 
-		console.log("[CR Premium] Optimizando página para móvil...");
-		//Verifique cuántos videos deslizantes caben en la pantalla.
-		width = document.body.offsetWidth;
-		var carousel_move_times = 0;
-		var carousel_videos_count = 0;
-		var carousel_arrow_limit = 0;
-
-		switch (true) {
-			case (width < 622 && width > 506):
-				carousel_move_times = 4;
-				break;
-			case (width < 506 && width > 390):
-				carousel_move_times = 3;
-				break;
-			case (width < 390 && width > 274):
-				carousel_move_times = 2;
-				break;
-			case (width < 274 && width > 0):
-				carousel_move_times = 1;
-				break;
-			default:
-				carousel_move_times = 5;
-		}
-		//Verifique cuántos videos hay en el control deslizante
-		function getChildNodes(node) {
-		    var children = new Array();
-		    for(var child in node.childNodes) {
-		        if(node.childNodes[child].nodeName == "DIV" && node.childNodes[child].attributes.media_id != null) {
-		            children.push(child);
-		        }
-		    }
-		    return children;
-		}
-		carousel_videos_count = getChildNodes(document.body.querySelector('div.collection-carousel-scrollable'));
-
-//Obtenga el script (porque sigue cambiando allí, siempre debe obtener dnv)
-		var carousel_arrow_limit = Number(pegaString(
-			document.body.querySelector('div.white-wrapper.container-shadow.large-margin-bottom').childNodes[3].innerHTML
-			, "Math.min", ","
-		).replace("(", ""));
-
-		var carousel_script = document.body.querySelector('div.white-wrapper.container-shadow.large-margin-bottom').childNodes[3].innerText
-		.replace(".data()['first_visible'] - 5", ".data()['first_visible'] - " + carousel_move_times)
-		.replace(".data()['first_visible'] + 5", ".data()['first_visible'] + " + carousel_move_times)
-		.replace("Math.min(" + carousel_arrow_limit + ",", "Math.min(" + (carousel_videos_count.length - carousel_move_times) + ",")
-		.replace(".data()['first_visible'] < " + carousel_arrow_limit, ".data()['first_visible'] < " + (carousel_videos_count.length - carousel_move_times))
-		.replace(".data()['first_visible'] >= " + carousel_arrow_limit, ".data()['first_visible'] >= " + (carousel_videos_count.length - carousel_move_times))
-		.replace(".data()['first_visible'] >= " + carousel_arrow_limit, ".data()['first_visible'] >= " + (carousel_videos_count.length - carousel_move_times));
-
-		//console.log("carousel_script = " + carousel_script);
-		//console.log("carousel_move_times = " + carousel_move_times);
-		//console.log("carousel_videos_count = " + carousel_videos_count.length);
-		//console.log("carousel_arrow_limit = " + carousel_arrow_limit);
-
-		//Elimine el script de los botones actuales, para eliminar la función anterior al hacer clic.
-		var old_element = document.querySelector(".collection-carousel-leftarrow");
-		var new_element = old_element.cloneNode(true);
-		old_element.parentNode.replaceChild(new_element, old_element);
-
-		var old_element = document.querySelector(".collection-carousel-rightarrow");
-		var new_element = old_element.cloneNode(true);
-		old_element.parentNode.replaceChild(new_element, old_element);
-
-		//Agrega el script modificado.
-		var head = document.getElementsByTagName('head')[0];
-		var script = document.createElement('script');
-		script.type = 'text/javascript';
-		script.onload = function() {
-		    callFunctionFromScript();
-		}
-		script.text = carousel_script;
-		head.appendChild(script);
-
-		//Haz que el video sea pequeño nuevamente en el primer episodio
-		if(document.getElementById('showmedia_video_box_wide') != null) {
-			document.getElementById('showmedia_video_box_wide').id = 'showmedia_video_box';
-		}
-
-		//Desbuga a seta de avançar o slider.
-		if(document.body.querySelector('div.collection-carousel-scrollable').lastElementChild.childNodes[1] != undefined) {
-			if(document.body.querySelector('div.collection-carousel-scrollable').lastElementChild.childNodes[1].classList.value.indexOf('collection-carousel-media-link-current') == -1) {
-				if(carousel_move_times == 4) {
-					if(document.body.querySelector('div.collection-carousel-scrollable').lastElementChild.previousElementSibling.childNodes[1].classList.value.indexOf('collection-carousel-media-link-current') == -1) {
-						document.body.querySelector('a.collection-carousel-rightarrow').classList = "collection-carousel-arrow collection-carousel-rightarrow";
-					}
-				}else{
-					document.body.querySelector('a.collection-carousel-rightarrow').classList = "collection-carousel-arrow collection-carousel-rightarrow";
-				}
-			}else{
-				if(carousel_move_times == 2){
-					document.body.querySelector('a.collection-carousel-rightarrow').classList = "collection-carousel-arrow collection-carousel-rightarrow";
-				}
-			}
-		}
+        callback()
+      }
+      else if (tries > 250) clearInterval(finishRemove)
+      else tries++
+    }, 20)
+  } else if (query(element) != null) {
+    console.log(`[CR Premium] Quitando ${name}...`);
+    query(element).style.display = 'none';
+  }
 }
 
 //función que cambia el reproductor a uno más simple.
 function importPlayer() {
+  var HTML = document.documentElement.innerHTML;
+  console.log("[CR Old] Eliminando reproductor de Crunchyroll...");
+  var elem = document.getElementById('showmedia_video_player');
+  elem.parentNode.removeChild(elem);
 
-		//Eliminar reproductor de Crunchyroll.
-		console.log("[CR Premium] Eliminando reproductor de Crunchyroll...");
-		var elem = document.getElementById('showmedia_video_player');
-    	elem.parentNode.removeChild(elem);
+  console.log("[CR Old] Obteniendo datos de la transmisión...");
+  var video_config_media = JSON.parse(pegaString(HTML, "vilos.config.media = ", ";"));
 
-    	//Obtenga los datos de transmisión de la variable presente en el html.
-		console.log("[CR Premium] Obteniendo datos de la transmisión...");
-		var video_config_media = JSON.parse(pegaString(HTML, "vilos.config.media = ", ";"));
+  //Eliminar la nota principal sobre probar premium
+  //Eliminar las advertencias de que el video no se puede ver
+  //Eliminar sugerencia para registrarse para la prueba gratuita
+  remove(".freetrial-note", "Free Trial Note")
+  remove(".showmedia-trailer-notice", "Trailer Notice")
+  remove("#showmedia_free_trial_signup", "Free Trial Signup")
 
-		//Añadir iframe con jwplayer
-    	console.log("[CR Premium] Agregando jwplayer...");
-    	ifrm = document.createElement("iframe");
-    	ifrm.setAttribute("id", "frame"); 
-		ifrm.setAttribute("src", "https://virtualox-sys.github.io/reproductor-crp/"); 
-		ifrm.setAttribute("width","100%");
-		ifrm.setAttribute("height","100%");
-		ifrm.setAttribute("frameborder","0");
-		ifrm.setAttribute("scrolling","no");
-		ifrm.setAttribute("allowfullscreen","allowfullscreen");
-		ifrm.setAttribute("allow","autoplay; encrypted-media *");
+  // Simula la interacción del usuario para que sea a pantalla completa automáticamente
+  // var element = document.getElementById("template_scroller");
+  // if (element) element.click();
 
-		//Elimina las advertencias de que el video no se puede ver.
-		if(document.body.querySelector("#showmedia_video_box") != null){
-			document.body.querySelector("#showmedia_video_box").appendChild(ifrm);
-		}else{
-			document.body.querySelector("#showmedia_video_box_wide").appendChild(ifrm);
-		}
+  const appendTo = query("#showmedia_video_box") || query("#showmedia_video_box_wide")
+  const series = document.querySelector('meta[property="og:title"]');
+  const up_next = document.querySelector('link[rel=next]');
 
-		if(document.body.querySelector(".showmedia-trailer-notice") != null){
-			document.body.querySelector(".showmedia-trailer-notice").style.textDecoration = "line-through";
-		}
-		if(document.body.querySelector("#showmedia_free_trial_signup") != null){
-			document.body.querySelector("#showmedia_free_trial_signup").style.display = "none";
-		}
+  var message = {
+    'video_config_media': [JSON.stringify(video_config_media)],
+    'lang': [pegaString(HTML, 'LOCALE = "', '",')],
+    'series': series ? series.content : undefined,
+    'up_next': up_next ? up_next.href : undefined,
+  }
 
-		//Al cargar el iframe, envía un mensaje al iframe con los datos de la transmisión.
-		ifrm.onload = function(){
-			ifrm.contentWindow.postMessage({
-           		'video_config_media': [JSON.stringify(video_config_media)],
-           		'lang': [pegaString(HTML, 'LOCALE = "', '",')]
-        	},"*");
-	    };
-
-		console.log(video_config_media);
-
-		//Cuando termine de modificar la página, verifique si es móvil y optimice la página.
-		if(width < 796) {
-			optimize_for_mobile();
-		}
+  console.log("[CR Old] Agregando jwplayer...");
+  addPlayer(appendTo, message)
 }
-//función al cargar la página.
+
+//Reproductor de renderizado en versión beta
+function importBetaPlayer(ready = false) {
+  var videoPlayer = query('.video-player') || query('#frame');
+  if (!ready) {
+    setTimeout(() => importBetaPlayer(!!videoPlayer), 100);
+    return;
+  }
+
+  console.log("[CR Beta] Eliminando reproductor de Crunchyroll...");
+  remove('.video-player-placeholder', 'Video Placeholder')
+  const appendTo = videoPlayer.parentNode;
+  appendTo.removeChild(videoPlayer);
+
+  console.log("[CR Beta] Obteniendo datos de la transmisión...");
+  var external_lang = preservedState.localization.locale.toLowerCase()
+  var ep_lang = preservedState.localization.locale.replace('-', '')
+  var ep_id = preservedState.watch.id
+  var ep = preservedState.content.byId[ep_id]
+  if (!ep) { window.location.reload(); return; }
+  var series_slug = ep.episode_metadata.series_slug_title
+  var external_id = ep.external_id.substr(4)
+  var old_url = `https://www.crunchyroll.com/${external_lang}/${series_slug}/episode-${external_id}`
+  var up_next = document.querySelector('[data-t="next-episode"] > a')
+  var playback = ep.playback
+
+  var message = {
+    'playback': playback,
+    'old_url': old_url,
+    'lang': ep_lang,
+    'up_next': up_next ? up_next.href : undefined,
+  }
+
+  console.log("[CR Beta] Agregando jwplayer...");
+  console.log("[CR Beta] Antigua URL:", old_url);
+  addPlayer(appendTo, message, true)
+}
+
+function addPlayer(element, playerInfo, beta = false) {
+  console.log("[CR Premium] Agregando jwplayer...");
+  var ifrm = document.createElement("iframe");
+  ifrm.setAttribute("id", "frame");
+  ifrm.setAttribute("src", "https://virtualox-sys.github.io/reproductor-crp/");
+  ifrm.setAttribute("width", "100%");
+  ifrm.setAttribute("height", "100%");
+  ifrm.setAttribute("frameborder", "0");
+  ifrm.setAttribute("scrolling", "no");
+  ifrm.setAttribute("allowfullscreen", "allowfullscreen");
+  ifrm.setAttribute("allow", "autoplay; encrypted-media *");
+
+  element.appendChild(ifrm)
+
+  chrome.storage.sync.get(['forcemp4', 'aseguir', 'cooldown', 'webvideocaster'], function (items) {
+    ifrm.onload = function () {
+      playerInfo['webvideocaster'] = items.webvideocaster === undefined ? false : items.webvideocaster;
+      playerInfo['up_next_cooldown'] = items.cooldown === undefined ? 5 : items.cooldown;
+      playerInfo['up_next_enable'] = items.aseguir === undefined ? true : items.aseguir;
+      playerInfo['force_mp4'] = items.forcemp4 === undefined ? false : items.forcemp4;
+      playerInfo['version'] = '1.2.1';
+      playerInfo['noproxy'] = true;
+      playerInfo['beta'] = beta;
+      ifrm.contentWindow.postMessage(playerInfo, "*");
+    };
+  });
+}
+
+//Función al cargar la página.
 function onloadfunction() {
-	//Si estás en la página que solicita una aplicación, te redirige a casa.
-    if(window.location.href == "https://www.crunchyroll.com/interstitial/android") {
-       	window.location.href = "https://www.crunchyroll.com/interstitial/android?skip=1";
-    }
-
-	//Agregar metaetiqueta para poder optimizar para dispositivos móviles.
-	var metaTag = document.createElement('meta');
-	metaTag.name = "viewport"
-	metaTag.content = "width=device-width, initial-scale=1.0, shrink-to-fit=no, user-scalable=no"
-	document.getElementsByTagName('head')[0].appendChild(metaTag);
-
-	//Ir al principio de la página.
-	window.scrollTo(0, 0);
-
-	if(pegaString(HTML, "vilos.config.media = ", ";") != null){
-		importPlayer();
-	}
+  var HTML = document.documentElement.innerHTML;
+  if (pegaString(HTML, "vilos.config.media = ", ";") != null) {
+    importPlayer(); // old CR
+  } else if (preservedState != null) {
+    importBetaPlayer(); // beta CR
+    remove(".erc-modal-portal > .overlay > .content-wrapper", "Free Trial Modal", true, () => document.body.classList = [])
+    remove(".erc-watch-premium-upsell", "Premium Sidebar", true)
+    registerChangeEpisode();
+  }
 }
-document.addEventListener("DOMContentLoaded", onloadfunction());
+
+//Función para actualizar la página al cambiar de episodio por UI beta
+var currentURL = window.location.href;
+
+function registerChangeEpisode() {
+  setInterval(async () => {
+    if (currentURL !== window.location.href) {
+      currentURL = window.location.href
+      if (currentURL.includes("/watch/")) {
+        const HTML = await fetch(currentURL)
+        console.log("[CR Beta] Buscando nuevos INITIAL_STATE")
+        preservedState = JSON.parse(pegaString(HTML, "__INITIAL_STATE__ = ", ";"))
+        importBetaPlayer(false)
+      }
+    }
+  }, 50)
+}
+
+document.addEventListener("DOMContentLoaded", onloadfunction, false);
+document.onreadystatechange = function () {
+  if (document.readyState === "interactive") {
+    console.log("[CR Beta] Searching for INITIAL_STATE")
+    const HTML = document.documentElement.innerHTML
+    preservedState = JSON.parse(pegaString(HTML, "__INITIAL_STATE__ = ", ";"))
+  }
+
+  const crBetaStyle = document.createElement('style');
+  crBetaStyle.innerHTML = `.video-player-wrapper {
+    margin-top: 2rem;
+    margin-bottom: calc(-3vh - 7vw);
+    height: 57.25vw !important;
+    max-height: 82vh !important;
+  }`;
+  document.head.appendChild(crBetaStyle);
+}
+
+function fetch(url) {
+  return new Promise(async (resolve, reject) => {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.withCredentials = true;
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState == 4)
+        if (xhr.status == 200) resolve(xhr.responseText)
+        else reject(xhr.statusText)
+    }
+    xhr.send();
+  })
+}
